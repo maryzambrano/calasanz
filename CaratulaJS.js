debb1ed3aea1fb6c1c84d00bbfacede4222d8b0f
Marynellis Zambrano <132614436+maryzambrano@users.noreply.github.com>
2025-02-05 16:30:56 -0300

    Add files via upload

--- CaratulaJS.js
+++ CaratulaJS.js
@@ -1,5 +1,7 @@
+
 function generarCaratula() {
     // Obtener los valores ingresados en el formulario
+    document.getElementById('caratula').style.display = 'none';
     const nombre = document.getElementById('nombre').value.trim();
     const grado = document.getElementById('grado').value.trim();
     const materia = document.getElementById('materia').value.trim();
@@ -7,32 +9,29 @@ function generarCaratula() {
 
     // Verificar si algún campo está vacío
     if (!nombre || !grado || !materia || !docente) {
-        alert('Algunos datos están incompletos. La carátula mostrará información incompleta.');
+        alert('Por favor, complete todos los campos antes de generar la carátula.');
+        return; // No continúa con la generación de la carátula
     }
 
     // Insertar los valores en la carátula
-    document.getElementById('caratulaNombre').textContent = nombre || "Sin especificar";
-    document.getElementById('caratulaGrado').textContent = grado || "Sin especificar";
-    document.getElementById('caratulaMateria').textContent = materia || "Sin especificar";
-    document.getElementById('caratulaDocente').textContent = docente || "Sin especificar";
-
-    // Aplicar clase de previsualización
-    const caratula = document.getElementById('caratula');
-    caratula.classList.add('print-preview');
+    document.getElementById('caratulaNombre').textContent = nombre;
+    document.getElementById('caratulaGrado').textContent = grado;
+    document.getElementById('caratulaMateria').textContent = materia;
+    document.getElementById('caratulaDocente').textContent = docente;
 
     // Mostrar la carátula y el botón de imprimir
-    caratula.style.display = 'block';
+    document.getElementById('caratula').style.display = 'block';
     document.getElementById('printButton').style.display = 'block';
+    
 }
-
 
 function limpiarFormulario() {
     // Limpiar los campos del formulario
     document.getElementById('studentForm').reset();
 
     // Ocultar la carátula y el botón de imprimir
-    //document.getElementById('caratula').style.display = 'none';
-    //document.getElementById('printButton').style.display = 'none';
+    document.getElementById('caratula').style.display = 'none';
+    document.getElementById('printButton').style.display = 'none';
 }
 
 function imprimirPDF() {
--- indexScript.js
+++ indexScript.js
@@ -1,11 +1,56 @@
 const today = new Date();
 let currentMonth = today.getMonth();
-let currentYear = today.getFullYear();
+let currentYear = 2025; // Fijamos el año en 2025
 const events = [
-    { date: "2025-01-22", type: "efemerides", description: "Día de la Soberanía Nacional" },
-    { date: "2025-01-25", type: "evaluaciones", description: "Examen de Matemáticas" },
-    { date: "2025-01-28", type: "otros", description: "Reunión de padres" },
+    //Efemérides y Solemnidades
+    { date: "2025-01-01", type: "efemerides", description: "Año nuevo" },
+    { date: "2025-03-03", type: "efemerides", description: "Carnaval" },
+    { date: "2025-03-04", type: "efemerides", description: "Carnaval" },
+    { date: "2025-03-05", type: "efemerides", description: "Miércoles de Cenizas" },
+    { date: "2025-03-24", type: "efemerides", description: "Día Nacional de la Memoria por la Verdad y la Justicia" },
+    { date: "2025-04-02", type: "efemerides", description: "Día del Veterano y de los Caídos en la Guerra de Malvinas" },
+    { date: "2025-04-13", type: "efemerides", description: "Domingo de Ramos" },
+    { date: "2025-04-17", type: "efemerides", description: "Jueves Santo" },
+    { date: "2025-04-18", type: "efemerides", description: "Viernes Santo" },
+    { date: "2025-04-19", type: "efemerides", description: "Sábado de Gloria" },
+    { date: "2025-04-20", type: "efemerides", description: "Resurrección del Señor" },
+    { date: "2025-05-01", type: "efemerides", description: "Día del Trabajador" },
+    { date: "2025-05-08", type: "efemerides", description: "Ntra. Sra. De Luján" },
+    { date: "2025-05-08", type: "efemerides", description: "Virgen de las Escuelas Pías" },
+    { date: "2025-05-25", type: "efemerides", description: "Día de la Revolución de Mayo" },
+    { date: "2025-06-08", type: "efemerides", description: "Pentecostés" },
+    { date: "2025-06-16", type: "efemerides", description: "Paso a la Inmortalidad del General Martín Güemes" },
+    { date: "2025-06-20", type: "efemerides", description: "Paso a la Inmortalidad del General Manuel Belgrano" },
+    { date: "2025-07-09", type: "efemerides", description: "Día de la Independencia" },
+    { date: "2025-08-17", type: "efemerides", description: "Paso a la Inmortalidad del Gral. José de San Martín" },
+    { date: "2025-08-25", type: "efemerides", description: "San José de Calasanz" },
+    { date: "2025-10-12", type: "efemerides", description: "Día del Respeto a la Diversidad Cultural" },
+    { date: "2025-11-24", type: "efemerides", description: "Día de la Soberanía Nacional" },
+    { date: "2025-12-08", type: "efemerides", description: "Día de la Inmaculada Concepción de María" },
+    { date: "2025-12-25", type: "efemerides", description: "Navidad" },
+    //Otros eventos
+    { date: "2025-02-10", type: "otros", description: "Inicio Ciclo escolar 2025" },
+    { date: "2025-02-24", type: "otros", description: "Inicio de clases primaria" },
+    { date: "2025-03-05", type: "otros", description: "Inicio de clases secundaria" },
+    { date: "2025-05-02", type: "otros", description: "Turístico" },
+    { date: "2025-07-21", type: "otros", description: "Receso invernal" },
+    { date: "2025-07-22", type: "otros", description: "Receso invernal" },
+    { date: "2025-07-23", type: "otros", description: "Receso invernal" },
+    { date: "2025-07-24", type: "otros", description: "Receso invernal" },
+    { date: "2025-07-25", type: "otros", description: "Receso invernal" },
+    { date: "2025-07-28", type: "otros", description: "Receso invernal" },
+    { date: "2025-07-29", type: "otros", description: "Receso invernal" },
+    { date: "2025-07-30", type: "otros", description: "Receso invernal" },
+    { date: "2025-07-31", type: "otros", description: "Receso invernal" },
+    { date: "2025-08-01", type: "otros", description: "Receso invernal" },
+    { date: "2025-08-15", type: "otros", description: "Turístico" },
+    { date: "2025-11-21", type: "otros", description: "Turístico" },
+    { date: "2025-12-19", type: "otros", description: "Finaliza el Ciclo lectivo 2025" },
+    //Evaluaciones
+    { date: "2025-01-25", type: "evaluaciones", description: "Quiz (Unidad I)" },
+    { date: "2025-05-23", type: "evaluaciones", description: "Quiz (Unidad II)" }
 ];
+
 const currentMonthYear = document.getElementById("currentMonthYear");
 const calendarGrid = document.getElementById("calendarGrid");
 const prevMonth = document.getElementById("prevMonth");
@@ -41,18 +86,27 @@ function renderCalendar(month, year) {
         day.textContent = i;
         calendarGrid.appendChild(day);
     }
+    
+    // Deshabilitar botones si se llega a los límites
+    prevMonth.disabled = (currentMonth === 0);
+    nextMonth.disabled = (currentMonth === 11);
+
+    prevMonth.style.backgroundColor = prevMonth.disabled ? "#d3d3d3" : "";
+    nextMonth.style.backgroundColor = nextMonth.disabled ? "#d3d3d3" : "";
 }
 
 prevMonth.addEventListener("click", () => {
-    currentMonth = (currentMonth - 1 + 12) % 12;
-    if (currentMonth === 11) currentYear--;
-    renderCalendar(currentMonth, currentYear);
+    if (currentMonth > 0) {
+        currentMonth--;
+        renderCalendar(currentMonth, currentYear);
+    }
 });
 
 nextMonth.addEventListener("click", () => {
-    currentMonth = (currentMonth + 1) % 12;
-    if (currentMonth === 0) currentYear++;
-    renderCalendar(currentMonth, currentYear);
+    if (currentMonth < 11) {
+        currentMonth++;
+        renderCalendar(currentMonth, currentYear);
+    }
 });
 
 filterType.addEventListener("change", (e) => {
@@ -61,3 +115,5 @@ filterType.addEventListener("change", (e) => {
 });
 
 renderCalendar(currentMonth, currentYear);
+
+
